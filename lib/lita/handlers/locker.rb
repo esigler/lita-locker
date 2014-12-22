23d1b92247bb0edcee041b0cf5634f4ba5db77c6
Eric Sigler <me@esigler.com>
2014-12-21 21:41:40 -0800

    Friends dont let friends spaghetti code

--- lib/lita/handlers/locker.rb
+++ lib/lita/handlers/locker.rb
@@ -47,32 +47,7 @@ module Lita
         return response.reply('(failed) ' + t('label.no_resources', name: name)) unless m.count > 0
         return response.reply('(successful) ' + t('label.lock', name: name)) if lock_label!(name, response.user, nil)
 
-        l = label(name)
-        if l['state'] == 'locked'
-          o = Lita::User.find_by_id(l['owner_id'])
-          if o.mention_name
-            response.reply('(failed) ' + t('label.owned_mention',
-                                           name: name,
-                                           owner_name: o.name,
-                                           owner_mention: o.mention_name))
-          else
-            response.reply('(failed) ' + t('label.owned',
-                                           name: name,
-                                           owner_name: o.name))
-          end
-        else
-          msg = '(failed) ' + t('label.dependency') + "\n"
-          deps = []
-          label_membership(name).each do |resource_name|
-            resource = resource(resource_name)
-            u = Lita::User.find_by_id(resource['owner_id'])
-            if resource['state'] == 'locked'
-              deps.push "#{resource_name} - #{u.name}"
-            end
-          end
-          msg += deps.join("\n")
-          response.reply(msg)
-        end
+        response.reply(show_ownership(name))
       end
 
       def unlock(response)
@@ -81,22 +56,7 @@ module Lita
         l = label(name)
         return response.reply('(successful) ' + t('label.is_unlocked', name: name)) if l['state'] == 'unlocked'
 
-        if response.user.id == l['owner_id']
-          unlock_label!(name)
-          response.reply('(successful) ' + t('label.unlock', name: name))
-        else
-          o = Lita::User.find_by_id(l['owner_id'])
-          if o.mention_name
-            response.reply('(failed) ' + t('label.owned_mention',
-                                           name: name,
-                                           owner_name: o.name,
-                                           owner_mention: o.mention_name))
-          else
-            response.reply('(failed) ' + t('label.owned',
-                                           name: name,
-                                           owner_name: o.name))
-          end
-        end
+        response.reply(attempt_unlock(name, l, response.user))
       end
 
       def steal(response)
@@ -104,17 +64,57 @@ module Lita
         return response.reply('(failed) ' + t('subject.does_not_exist', name: name)) unless label_exists?(name)
         l = label(name)
         return response.reply(t('steal.already_unlocked', label: name)) unless l['state'] == 'locked'
+
+        response.reply(attempt_steal(name, l, response.user))
+      end
+
+      private
+
+      def show_ownership(name)
+        l = label(name)
+        return show_dependencies(name) unless l['state'] == 'locked'
         o = Lita::User.find_by_id(l['owner_id'])
-        if o.id != response.user.id
-          unlock_label!(name)
-          lock_label!(name, response.user, nil)
-          mention = o.mention_name ? "(@#{o.mention_name})" : ''
-          response.reply('(successful) ' + t('steal.stolen',
-                                             label: name,
-                                             old_owner: o.name,
-                                             mention: mention))
+        if o.mention_name
+          return '(failed) ' + t('label.owned_mention', name: name, owner_name: o.name, owner_mention: o.mention_name)
         else
-          response.reply(t('steal.self'))
+          return '(failed) ' + t('label.owned', name: name, owner_name: o.name)
+        end
+      end
+
+      def show_dependencies(name)
+        msg = '(failed) ' + t('label.dependency') + "\n"
+        deps = []
+        label_membership(name).each do |resource_name|
+          resource = resource(resource_name)
+          u = Lita::User.find_by_id(resource['owner_id'])
+          if resource['state'] == 'locked'
+            deps.push "#{resource_name} - #{u.name}"
+          end
+        end
+        msg += deps.join("\n")
+        msg
+      end
+
+      def attempt_steal(name, label, user)
+        o = Lita::User.find_by_id(label['owner_id'])
+        return t('steal.self') if o.id == user.id
+        unlock_label!(name)
+        lock_label!(name, user, nil)
+        mention = o.mention_name ? "(@#{o.mention_name})" : ''
+        '(successful) ' + t('steal.stolen', label: name, old_owner: o.name, mention: mention)
+      end
+
+      def attempt_unlock(name, label, user)
+        if user.id == label['owner_id']
+          unlock_label!(name)
+          return '(successful) ' + t('label.unlock', name: name)
+        else
+          o = Lita::User.find_by_id(label['owner_id'])
+          if o.mention_name
+            return '(failed) ' + t('label.owned_mention', name: name, owner_name: o.name, owner_mention: o.mention_name)
+          else
+            return '(failed) ' + t('label.owned', name: name, owner_name: o.name)
+          end
         end
       end
     end
