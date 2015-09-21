fecc071aa92ddd75b1684c4c406d52fce7d98cbb
Tristan Chong <tristanchong@gmail.com>
2015-09-21 10:47:38 -0700

    [#45] Allow status checks for multiple labels with wildcard operator
    With this change, a user will be able to send the command `locker status
    foo*` and receive information about the labels foo1, foo2, foo3, etc.

--- lib/locker/regex.rb
+++ lib/locker/regex.rb
@@ -2,13 +2,14 @@
 module Locker
   # Regex definitions
   module Regex
-    LABEL_REGEX    = /(?<label>[\.\w\s-]+)(\s)?/
-    LABELS_REGEX   = /(?<labels>[\.\w\s-]+(?:,\s*[\.\w\s-]+)*)(\s)?/
-    RESOURCE_REGEX = /(?<resource>[\.\w-]+)/
-    RESOURCES_REGEX = /(?<resources>[\.\w-]+(?:,\s*[\.\w-]+)*)/
-    COMMENT_REGEX  = /(\s\#.+)?/
-    LOCK_REGEX     = /\(lock\)\s/i
-    USER_REGEX     = /(?:@)?(?<username>[\w\s-]+)/
-    UNLOCK_REGEX   = /(?:\(unlock\)|\(release\))\s/i
+    LABEL_REGEX           = /(?<label>[\.\w\s-]+)(\s)?/
+    LABEL_WILDCARD_REGEX  = /(?<label>[\.\*\w\s-]+)(\s)?/
+    LABELS_REGEX          = /(?<labels>[\.\w\s-]+(?:,\s*[\.\w\s-]+)*)(\s)?/
+    RESOURCE_REGEX        = /(?<resource>[\.\w-]+)/
+    RESOURCES_REGEX       = /(?<resources>[\.\w-]+(?:,\s*[\.\w-]+)*)/
+    COMMENT_REGEX         = /(\s\#.+)?/
+    LOCK_REGEX            = /\(lock\)\s/i
+    USER_REGEX            = /(?:@)?(?<username>[\w\s-]+)/
+    UNLOCK_REGEX          = /(?:\(unlock\)|\(release\))\s/i
   end
 end
